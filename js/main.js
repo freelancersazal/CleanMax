fe5e0c53656b9f9df3e4dcded758943a1fd82a62
freelancersazal <freelancersazal@gmail.com>
2022-08-22 18:21:43 +0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,127 @@
+$(function () {
+
+    "use strict";
+
+
+    //======menu fix js======   
+    if ($('.main_menu').offset() != undefined) {
+        var navoff = $('.main_menu').offset().top;
+        $(window).scroll(function () {
+            var scrolling = $(this).scrollTop();
+
+            if (scrolling > navoff) {
+                $('.main_menu').addClass('menu_fix');
+            } else {
+                $('.main_menu').removeClass('menu_fix');
+            }
+        });
+    }
+
+    //=======SELECT JS====== 
+    $('#select_js').niceSelect();
+    $('#select_js2').niceSelect();
+
+
+    //=========main.js=========   
+    $('.counter').countUp();
+
+
+    //=======CATEGORY SLIDER====== 
+    $('.category_slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 5,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+
+    //=======CATEGORY SLIDER====== 
+    $('.featured_service').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+});
