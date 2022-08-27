b84bb8bac091d34a57ed6b30257792eeddb90ade
freelancersazal <freelancersazal@gmail.com>
2022-08-27 18:03:53 +0600

    added two new pages

--- js/main.js
+++ js/main.js
@@ -20,6 +20,8 @@ $(function () {
     //=======SELECT JS====== 
     $('#select_js').niceSelect();
     $('#select_js2').niceSelect();
+    $('#select_js3').niceSelect();
+    $('#select_js4').niceSelect();
 
 
     //=========main.js=========   
